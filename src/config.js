fa73657d471b02410d5c0916ea602b16b2a36ff6
Susana Rodriguez <susana.rodriguez@bbc.co.uk>
2020-10-31 15:14:56 +0000

    NO_TICKET - Add AWS amplify

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "eu-west-2",
+    BUCKET: "notes.me",
+  },
+  apiGateway: {
+    REGION: "eu-west-2",
+    URL: "https://4i58u83qqb.execute-api.eu-west-2.amazonaws.com/prod",
+  },
+  cognito: {
+    REGION: "eu-west-2",
+    USER_POOL_ID: "eu-west-2_8fyfyhjTw",
+    APP_CLIENT_ID: "2o3h5rs48b6bne23uc8hg7h856",
+    IDENTITY_POOL_ID: "eu-west-2:f01b0365-6f53-4dce-b303-66f0b3fdb3ed",
+  },
+};
