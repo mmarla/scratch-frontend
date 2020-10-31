fd81d5db8ac0d4472d6fa82d4ddfc90120376aa6
Susana Rodriguez <susana.rodriguez@bbc.co.uk>
2020-10-31 15:06:06 +0000

    NO_TICKET - Basic routing setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Navbar } from "react-bootstrap";
+import Routes from "./Routes";
+
 import "./App.css";
 
 function App() {
@@ -14,6 +16,7 @@ function App() {
           <Navbar.Toggle />
         </Navbar.Header>
       </Navbar>
+      <Routes />
     </div>
   );
 }
