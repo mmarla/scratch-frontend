38be2c6e56ebc7b562c0b60391bccf32364e7e66
Susana Rodriguez <susana.rodriguez@bbc.co.uk>
2020-11-13 09:22:04 +0000

    Refactor Login to use custom hooks

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -3,6 +3,7 @@ import { FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 import { Auth } from "aws-amplify";
 import { useHistory } from "react-router-dom";
 import { useAppContext } from "../libs/context";
+import { useFormFields } from "../libs/hooks";
 import { onError } from "../libs/errors";
 
 import LoaderButton from "../components/LoaderButton";
@@ -10,14 +11,16 @@ import LoaderButton from "../components/LoaderButton";
 import "./Login.css";
 
 export default function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [fields, handleFieldChange] = useFormFields({
+    email: "",
+    password: "",
+  });
   const [isLoading, setIsLoading] = useState(false);
   const { userHasAuthenticated } = useAppContext();
   const history = useHistory();
 
   function validateForm() {
-    return email.length > 0 && password.length > 0;
+    return fields.email.length > 0 && fields.password.length > 0;
   }
 
   async function handleSubmit(event) {
@@ -26,7 +29,7 @@ export default function Login() {
     setIsLoading(true);
 
     try {
-      await Auth.signIn(email, password);
+      await Auth.signIn(fields.email, fields.password);
       userHasAuthenticated(true);
       history.push("/");
     } catch (e) {
@@ -43,15 +46,15 @@ export default function Login() {
           <FormControl
             autoFocus
             type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            value={fields.email}
+            onChange={handleFieldChange}
           />
         </FormGroup>
         <FormGroup controlId="password" bsSize="large">
           <ControlLabel>Password</ControlLabel>
           <FormControl
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            value={fields.password}
+            onChange={handleFieldChange}
             type="password"
           />
         </FormGroup>
