56bae7767536ef2d1047213b7a6e0c0f52a2bef2
Susana Rodriguez <susana.rodriguez@bbc.co.uk>
2020-11-13 11:07:38 +0000

    Add proper submit to NewNote

--- src/containers/NewNote.js
+++ src/containers/NewNote.js
@@ -1,6 +1,8 @@
 import React, { useRef, useState } from "react";
 import Form from "react-bootstrap/Form";
 import { useHistory } from "react-router-dom";
+import { API } from "aws-amplify";
+
 import LoaderButton from "../components/LoaderButton";
 import { onError } from "../libs/errors";
 import config from "../config";
@@ -33,6 +35,20 @@ export default function NewNote() {
     }
 
     setIsLoading(true);
+
+    try {
+      await createNote({ content });
+      history.push("/");
+    } catch (e) {
+      onError(e);
+      setIsLoading(false);
+    }
+  }
+
+  function createNote(note) {
+    return API.post("notes", "/notes", {
+      body: note,
+    });
   }
 
   return (
