429aee99a3d4c405ff748f7dfed6d3c1cbadf33d
Susana Rodriguez <susana.rodriguez@bbc.co.uk>
2020-11-13 11:40:13 +0000

    Add Billing

--- src/config.js
+++ src/config.js
@@ -13,6 +13,8 @@ const config = {
     APP_CLIENT_ID: "2o3h5rs48b6bne23uc8hg7h856",
     IDENTITY_POOL_ID: "eu-west-2:f01b0365-6f53-4dce-b303-66f0b3fdb3ed",
   },
+  STRIPE_KEY:
+    "pk_test_51GCRmGC5ms5QLqs0FUgFZUfJuvkmIpzZqlxy4a6Nf72M6naHfj6hioPawlPRJjp6yWghhPiGfyIrNXRWgKyLSb3y00vKu956IC",
 };
 
 export default config;
